90e537aa9e552d97ffe0b74d16eac12c055f2552
titungpemba <pembatamang.m@gmail.com>
2020-03-14 13:13:17 +0545

    get output of : Name of the City. City latitude. City Longitude.

--- 5daysOWM.py
+++ 5daysOWM.py
@@ -6,4 +6,9 @@ base_url = "http://api.openweathermap.org/data/2.5/forecast"
 complete_url = base_url + "?q=" + name_of_the_place + "&appid=" + app_id 
 #print(complete_url)
 json_data = requests.get(complete_url).json()
-print(json_data)
+#print(json_data)
+name_of_the_city = json_data['city']['name']
+print(name_of_the_city)
+city_lat = json_data['city']['coord']['lat']
+city_lon = json_data['city']['coord']['lon']
+print("latitude : " + str(city_lat) + ", Longitude : " + str(city_lon))
