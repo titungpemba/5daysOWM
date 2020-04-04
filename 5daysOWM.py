dfb4651a848020778bab88078440f194b55dff01
titungpemba <pembatamang.m@gmail.com>
2020-04-04 09:52:48 +0545

    Successful complete of myproject_Administrator.

--- 5daysOWM.py
+++ 5daysOWM.py
@@ -2,6 +2,7 @@ import requests
 
 app_id = "d1f7f1d42ce9fe62fafc2798e6747635"
 name_of_the_place = input('Name of the place: ')
+
 base_url = "http://api.openweathermap.org/data/2.5/forecast"
 complete_url = base_url + "?q=" + name_of_the_place + "&appid=" + app_id 
 #print(complete_url)
